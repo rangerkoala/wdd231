5e5092bebb86dfa63f7a2537439a1c37b7af519b
rangerkoala <sebastiancamilopf@gmail.com>
2025-11-14 13:41:46 -0500

    json corrections

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -25,3 +25,36 @@ listbutton.addEventListener('click', () => {
     cards.classList.remove('grid');
 })
 
+function displayMembers(members) {
+    cards.innerHTML = "";
+
+    members.forEach(member => {
+        let card = document.createElement('section');
+
+        let logo = document.createElement('img');
+        let name = document.createElement('h2');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let website = document.createElement('a');
+
+        name.textContent = member.name;
+        address.textContent = member.address;
+        phone.textContent = member.phone;
+        website.innerHTML = `${member.website}`;
+        website.setAttribute('href', member.website);
+        website.setAttribute('target', '_blank');
+        logo.setAttribute('src', member.image);
+        logo.setAttribute('alt', `Logo of ${member.name}`);
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '150');
+        logo.setAttribute('height', 'auto');
+
+        card.appendChild(logo);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+
+        cards.appendChild(card);
+    });
+}
